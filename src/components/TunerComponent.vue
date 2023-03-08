ddd52173416ffb6c5289d56e5e35539b2f754bb7
MariusBoucard <marius.boucard--bocciarelli@insa-rennes.fr>
2023-03-08 23:50:21 +0000

    I made it, still have to see for data binding

--- src/components/TunerComponent.vue
+++ src/components/TunerComponent.vue
@@ -1,5 +1,5 @@
 <template>
-<div id="tuner" style="height:100px;background-color: red;">
+<div id="tuner" style="background-color: red;">
 
     
 </div>
@@ -7,15 +7,13 @@
 </template>
 
 <script>
-import '../../jstuner/public/assets/bundle.js'
+// import '../../jstuner/public/assets/bundle.js'
 import { JsTunerUI, Recorder } from 'jstuner-ui';
 
 export default {
 
     mounted() {
-        let recaptchaScript = document.createElement('script')
-      recaptchaScript.setAttribute('src', 'https://www.google.com/recaptcha/api.js')
-      document.head.appendChild(recaptchaScript)
+  
     const ui = new JsTunerUI(document.getElementById("tuner"));
   const recorder = new Recorder();
   recorder.onData = (wave, hz, note)=>{
@@ -24,7 +22,7 @@ export default {
   recorder.main();
   },
   beforeUnmount() {
-    this.tuner.stop()
+   
   },
 
 }
