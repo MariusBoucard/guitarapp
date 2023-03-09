f95883fce0711d31242c2851f200750584bdd30c
MariusBoucard <marius.boucard--bocciarelli@insa-rennes.fr>
2023-03-09 09:36:06 +0000

    got back the note name

--- src/components/TunerComponent.vue
+++ src/components/TunerComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>{{this.note}}</h1>
+    <h1>{{name()}}</h1>
 
     <div id="tuner" style="background-color: red;">
         
@@ -20,16 +20,22 @@ export default {
     return{
       note:"A"
 
-    }
+    }},
+    methods : {
+      name() {
+              const names = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+              const note12 = (this.note >= 0) ? this.note % 12 : this.note % 12 + 12;
+              var i = Math.floor((note12 + 0.5) % 12);
+              return names[i];
+            }
     },
-
     mounted() {
   
     const ui = new JsTunerUI(document.getElementById("tuner"));
   const recorder = new Recorder();
   recorder.onData = (wave, hz, note)=>{
     ui.draw(wave, hz, note);
-    this.note = note;
+    this.note = hzs;
   }
   recorder.main();
   },
