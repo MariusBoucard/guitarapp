55cf9a48bac1e08c1ba63054e0a1b62cf57364cf
MariusBoucard <marius.boucard--bocciarelli@insa-rennes.fr>
2023-03-30 19:22:22 +0100

    added image loader component

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
         <TuningComponent :allNotes="this.allNotes" v-show="this.settingsView" @diap="changeDiap( $event)" :diapason=this.diapason :notesColor=this.colors :notesnumber=this.nbnotes :notesval="this.allNotes" :tuningList=this.tuningList :cordesNumber=this.nbStrings></TuningComponent> 
         <TunerComponent @changenote="changeNote($event,note)" :notePlayed="this.notePlayed" ></TunerComponent>
         <PlaySoundComponent></PlaySoundComponent>
+        <LoadPictureComponent></LoadPictureComponent>
       </div>
     </div>
    
@@ -39,6 +40,7 @@ import ColorComponent from './components/ColorComponent.vue';
 import GammeFinderComponent from './components/GammeFinderComponent.vue';
 import TunerComponent from './components/TunerComponent.vue';
 import PlaySoundComponent from './components/PlaySoundComponent.vue';
+import LoadPictureComponent from './components/LoadPictureComponent.vue';
 
 export default {
   name: 'App',
@@ -49,8 +51,9 @@ export default {
     TuningComponent,
     ColorComponent,
     GammeFinderComponent,
-    PlaySoundComponent
-  },
+    PlaySoundComponent,
+    LoadPictureComponent
+},
   data () {
     return {
       nbfrettes : 24,
---
+++ src/components/LoadPictureComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div >
+      <input type="file" @change="handleFileChange">
+      <p></p>
+      <img :src="imageUrl" v-if="imageUrl" style="max-width:100%;max-height:100%">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        imageUrl: '',
+      };
+    },
+    methods: {
+      handleFileChange(event) {
+        const file = event.target.files[0];
+        if (!file || !file.type.startsWith('image/')) {
+          return;
+        }
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.imageUrl = reader.result;
+        };
+        reader.readAsDataURL(file);
+      },
+    },
+  };
+  </script>
+  
